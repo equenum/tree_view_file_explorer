9aaee6de4d10417e96c15a4e377ca891835c9d88
geoGitAm <71641416+geoGitAm@users.noreply.github.com>
2021-01-23 20:03:59 +0300

    initial commit; created WPF-project along with Class Library; added CaliburnMicro to the WPF project; implemented MVVM pattern.

---
+++ TreeViewFileExplorer/TreeViewFileExplorerLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TreeViewFileExplorerLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ TreeViewFileExplorer/TreeViewFileExplorerUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Caliburn.Micro;
+
+namespace TreeViewFileExplorerUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+    }
+}
